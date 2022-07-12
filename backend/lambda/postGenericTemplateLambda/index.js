5df3fd76d8e117763cb83ab556d1d5860cf81052
Arild Andreassen <arildsa@hotmail.com>
2022-07-11 18:30:41 -0600

    updated'

---
+++ backend/lambda/postGenericTemplateLambda/index.js
@@ -0,0 +1,25 @@
+var AWS = require("aws-sdk");
+AWS.config.update({ region: "us-west-1" });
+
+var dynamodb = new AWS.DynamoDB({ apiVersion: "2012-08-10" });
+
+exports.handler = async (event) => {
+  var params = {
+    TableName: "GenericTemplateTable",
+    Key: {
+      ID: {
+        S: "123213",
+      },
+    },
+  };
+
+  var result = await dynamodb.getItem(params).promise();
+  return {
+    statusCode: 200,
+    headers: {
+      my_header: "my_value",
+    },
+    body: JSON.stringify("post 2"),
+    isBase64Encoded: false,
+  };
+};
--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [value, setValue] = useState("");
+  useEffect(() => {
+    const url =
+      "https://c6cmtlha3k.execute-api.us-west-1.amazonaws.com/default/genericTemplate";
+    fetch(url, {
+      method: "get",
+      headers: {
+        "x-api-key": "SCKlRrGrBh6xhzQeOurTR1vVmeTMgY5vaDIb1udR",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => setValue(data));
+  });
+
   return (
     <div className="App">
-      <header className="App-header">This is the app</header>
+      <header className="App-header">This is the app {value}</header>
     </div>
   );
 }
